cbf44b14fe86cb17ac2125f46db3e12ba24bfdb5
Deep Pudasaini <45301075+deeppudasaini@users.noreply.github.com>
2021-10-06 20:09:13 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
-
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 }
